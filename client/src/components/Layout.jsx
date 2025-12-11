f4e26ee0e129798f10ace07d6668690a388068ce
Tyler Llorens <tylerllore@gmail.com>
2025-12-11 12:14:07 -0500

    feat: minor tweaks

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -24,18 +24,3 @@ function Layout() {
 }
 
 export default Layout;
-// import { Outlet } from "react-router-dom";
-// import Navbar from "./Navbar.jsx";
-
-// function Layout() {
-//   return (
-//     <div className="app-shell">
-//       <Navbar />
-//       <main style={{ padding: "1rem" }}>
-//         <Outlet />
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default Layout;
